f97aa2b5a60a1be6d0341775c1a45bfe83d1a6c6
lukabuz <l.buzaladze@hotmail.com>
2018-06-27 02:18:06 +0400

    model, migration, and main part of controller for replies- BUG FIXES

--- app/Http/Controllers/SubmissionsController.php
+++ app/Http/Controllers/SubmissionsController.php
@@ -16,7 +16,9 @@ class SubmissionsController extends Controller
     public function show(Request $request, $id){
     	$submission = Submission::findOrFail($id);
 
-    	$replies = Submission_reply::where('submission_id', $id)->orderBy('created_at', 'asc');
+    	$replies = Submission_reply::where('submission_id', $id)->orderBy('created_at', 'asc')->get();
+
+    	return $replies;
 
     	return view('submission')->with('submission', $submission)->with('replies', $replies);
     }
