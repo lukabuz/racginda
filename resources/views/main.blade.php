8bcb34ec8586ee6faf26acfc44295ddf7c7ad4a1
lukabuz <l.buzaladze@hotmail.com>
2018-06-26 13:29:20 +0400

    finishing touches

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -720,4 +720,15 @@
 
     });
   </script>
+
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-121429645-1"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-121429645-1');
+  </script>
+  
 </html>
