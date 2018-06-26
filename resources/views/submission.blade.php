02ccbe9602d5acb2db7e501b2707947fada563c9
lukabuz <l.buzaladze@hotmail.com>
2018-06-27 02:03:55 +0400

    model, migration, and main part of controller for replies- BUG FIXES

--- resources/views/submission.blade.php
+++ resources/views/submission.blade.php
@@ -700,7 +700,7 @@
 
     <div class="item">
       <div class="content">
-        <p>{!! preg_replace('!(((f|ht)tp(s)?://)[-a-zA-Zа-яА-Я()0-9@:%_+.~#?&;//=]+)!i', '<a href="$1" target="_blank">$1</a>', nl2br(e($submission->description))) !!}</p>
+        <p>{!! nl2br(e($submission->description)) !!}</p>
       </div>
       <div class="voting" data-id="{{$submission->id}}">
         @if($submission->votevalue() == 1)
