f56e7eeaba3268b34fedf5ffdc57d39cc6bb4cbb
lukabuz <l.buzaladze@hotmail.com>
2018-06-26 14:59:39 +0400

    finishing touches

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -792,14 +792,20 @@
     });
   </script>
 
-  <!-- Global site tag (gtag.js) - Google Analytics -->
-  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-121429645-1"></script>
   <script>
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-
-    gtag('config', 'UA-121429645-1');
-  </script>
+        (function(b, o, i, l, e, r) {
+            b.GoogleAnalyticsObject = l;
+            b[l] || (b[l] = function() {
+                (b[l].q = b[l].q || []).push(arguments)
+            });
+            b[l].l = +new Date;
+            e = o.createElement(i);
+            r = o.getElementsByTagName(i)[0];
+            e.src = '//www.google-analytics.com/analytics.js';
+            r.parentNode.insertBefore(e, r)
+        }(window, document, 'script', 'ga'));
+        ga('create', 'UA-121429645-1', 'auto');
+        
+    </script>
 
 </html>
