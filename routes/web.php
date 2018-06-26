9afe6d570a6cd4940001147c362c1f89f357aa65
lukabuz <l.buzaladze@hotmail.com>
2018-06-27 01:52:17 +0400

    model, migration, and main part of controller for replies

--- routes/web.php
+++ routes/web.php
@@ -15,9 +15,9 @@ Route::get('/', 'MainController@index');
 
 Route::post('/', 'MainController@submit');
 
-Route::get('/submission/{id}' 'SubmissionsController@show');
+Route::get('/submission/{id}', 'SubmissionsController@show');
 
-Route::post('/submission/{id}' 'SubmissionsController@show');
+Route::post('/submission/{id}', 'SubmissionsController@reply');
 
 Route::get('/api/vote/{id}/upvote', 'MainController@upvote');
 
